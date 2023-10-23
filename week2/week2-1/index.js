f7c88dc6f3b56b37a38b5f168389cc5a2b9a6ab4
ljh0608 <m98m@catholic.ac.kr>
2023-10-23 12:47:10 +0900

    feat: top_btn opacity change according to scroll change

---
+++ week2/week2-1/index.js
@@ -0,0 +1,13 @@
+
+const top_btn=document.querySelector(".top_btn");
+
+function handleScroll(){
+    const positionY=window.scrollY;
+    const windowY=window.innerHeight;
+
+    top_btn.style.opacity=`${positionY/(document.body.clientHeight - 
+        windowY)}`;
+
+}
+
+window.addEventListener("scroll",handleScroll);
