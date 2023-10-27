3937105a98c71591348629b5813c16420cb925d5
ljh0608 <m98m@catholic.ac.kr>
2023-10-27 16:08:31 +0900

    feat: 삭제버튼 기능 구현

--- week2/week2-2/index.js
+++ week2/week2-2/index.js
@@ -1,39 +1,11 @@
 import { INIT_BALANCE, HISTORY_LIST } from "./constant.js";
 
 let filteredHistory=HISTORY_LIST;
-/**
- * 나의자산 & 수입 & 지출 헤더
- */
-
-const totalMoney = filteredHistory.reduce((total, item) => {
-    if (item.type === "income") {
-        total += item.money;
-    } else {
-        total -= Math.abs(item.money);
-    }
-    return total;
-}, 0);
-const depositSection = document.querySelector('.deposit__section');
-const totalMoneyElement = depositSection.querySelector('p');
-
-const income=depositSection.querySelector(".income__article");
-const expend=depositSection.querySelector(".expend__article");
-
-const filterIncome=filteredHistory.filter(item=>item.type==="income");
-const totalIncome = filterIncome.reduce((total, item) => total + item.money, 0);
-
-const filterExpend=filteredHistory.filter(item=>item.type==="expend");
-const totalExpend = filterExpend.reduce((total, item) => total + item.money, 0);
-
-
-totalMoneyElement.textContent = totalMoney.toLocaleString(); // 숫자 포맷팅 (콤마 추가)
-income.textContent =`+ ${totalIncome.toLocaleString()}`; 
-expend.textContent=`- ${totalExpend.toLocaleString()}`
 
 
 
 /**
- * 수입 지출 목록 렌더링
+ * 수입 지출 목록 구현
  */
 
 const withdrawUl = document.querySelector('.withdraw__ul');
@@ -66,10 +38,50 @@ function createWithdrawItem(item) {
     return li;
 }
 
-filteredHistory.forEach(item => {
-    const listItem = createWithdrawItem(item);
-    withdrawUl.appendChild(listItem);
-});
+
+/**
+ * 렌더링 함수
+ */
+
+function renderWithdrawList(filteredHistory) {
+
+/**
+ * 나의자산 & 수입 & 지출 헤더
+ */
+    const totalMoney = filteredHistory.reduce((total, item) => {
+        if (item.type === "income") {
+            total += item.money;
+        } else {
+            total -= Math.abs(item.money);
+        }
+        return total;
+    }, 0);
+    const depositSection = document.querySelector('.deposit__section');
+    const totalMoneyElement = depositSection.querySelector('p');
+    
+    const income=depositSection.querySelector(".income__article");
+    const expend=depositSection.querySelector(".expend__article");
+    
+    const filterIncome=filteredHistory.filter(item=>item.type==="income");
+    const totalIncome = filterIncome.reduce((total, item) => total + item.money, 0);
+    
+    const filterExpend=filteredHistory.filter(item=>item.type==="expend");
+    const totalExpend = filterExpend.reduce((total, item) => total + item.money, 0);
+    
+    
+    totalMoneyElement.textContent = totalMoney.toLocaleString(); // 숫자 포맷팅 (콤마 추가)
+    income.textContent =`+ ${totalIncome.toLocaleString()}`; 
+    expend.textContent=`- ${totalExpend.toLocaleString()}`
+    
+    /**
+     * 내역리스트 렌더링
+     */
+    withdrawUl.innerHTML = '';
+    filteredHistory.forEach(item => {
+        const listItem = createWithdrawItem(item);
+        withdrawUl.appendChild(listItem);
+    });
+}
 
 
 /**
@@ -79,19 +91,11 @@ const income_checkbox=document.querySelector(".income-checkbox__input");
 const expend_checkbox=document.querySelector(".expend-checkbox__input");
 
 
-function renderWithdrawList(filteredHistory) {
-    withdrawUl.innerHTML = '';
-    filteredHistory.forEach(item => {
-        const listItem = createWithdrawItem(item);
-        withdrawUl.appendChild(listItem);
-    });
-}
-
 function updateFilteredHistory() {
     const showIncome = income_checkbox.checked;
     const showExpend = expend_checkbox.checked;
 
-     filteredHistory = HISTORY_LIST.filter(item => {
+     const filterCheckHistory = filteredHistory.filter(item => {
         if (showIncome && showExpend) {
             return true; 
         } else if (showIncome) {
@@ -102,50 +106,33 @@ function updateFilteredHistory() {
         return false; 
     });
 
-    renderWithdrawList(filteredHistory);
+    renderWithdrawList(filterCheckHistory);
 }
 
 
 income_checkbox.addEventListener('change', updateFilteredHistory);
  expend_checkbox.addEventListener('change', updateFilteredHistory);
-updateFilteredHistory(); 
+ updateFilteredHistory(); 
 
 
 /**
  * 삭제 버튼 기능 구현
  */
 
-const deleteBtn=document.querySelectorAll(".delete-btn");
 
-function deleteItem(listItem) {
-    withdrawUl.removeChild(listItem);
-    const itemName = listItem.querySelector('.title__article').textContent;
-    const itemType = listItem.querySelector('.history__article').textContent.substr(2);
-    console.log(itemName, itemType)
-    // filteredHistory= HISTORY_LIST.filter(item => !(item.name === itemName && item.type === itemType));
-    // updateFilteredHistory(); 
- 
-}
 
-deleteBtn.forEach((button) => {
-    button.addEventListener('click', function() {
-        const listItem = this.parentElement;
-        console.log(listItem);
-          deleteItem(listItem);
-    });
+withdrawUl.addEventListener('click', function(event) {
+    const target = event.target;
+    if (target.classList.contains('delete-btn')) {
+        const listItem = target.parentElement;
+        const index = Array.from(listItem.parentElement.children).indexOf(listItem);
+        
+        filteredHistory.splice(index, 1);
+        
+        renderWithdrawList(filteredHistory);
+    }
 });
-// deleteBtn.addEventListener("click",function(){
-//     const listItem = this.parentElement;
-//     console.log(listItem)
-//     // 해당 항목을 HISTORY_LIST에서 제거
-//     const itemName = listItem.querySelector('.title__article').textContent;
-//     const itemType = listItem.querySelector('.history__article').classList.contains('income') ? 'income' : 'expend';
-//     HISTORY_LIST.filter(item => !(item.name === itemName && item.type === itemType));
 
-//     // 필터링된 목록 다시 렌더링
-//     renderWithdrawList(HISTORY_LIST);
-// }
-// )
 
 
 
