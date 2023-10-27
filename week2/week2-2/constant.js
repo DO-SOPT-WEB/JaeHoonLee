bc5aca7833b5f39cea6324d775f47527519df39a
ljh0608 <m98m@catholic.ac.kr>
2023-10-27 13:48:43 +0900

    feat: constant data

---
+++ week2/week2-2/constant.js
@@ -0,0 +1,25 @@
+
+const INIT_BALANCE=0;
+const HISTORY_LIST=[
+    {
+        category:"식비",
+        name:"버거킹 와퍼",
+        money:-8400
+    }
+    ,   {
+        category:"교통",
+        name:"주차비",
+        money:-11000
+    }
+    ,   {
+        category:"알바비",
+        name:"아이엠파트너",
+        money:232000
+    }
+    ,   {
+        category:"알바비",
+        name:"디지털새싹 인공지능교육 강사",
+        money:656000
+    }
+  
+]
