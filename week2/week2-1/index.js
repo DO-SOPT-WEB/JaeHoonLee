60c1bb0bff1a017616dcdc68e998cc65269dc831
ljh0608 <m98m@catholic.ac.kr>
2023-11-05 15:00:41 +0900

    refactor: 코드 포맷 스타일 변경

--- week2/week2-1/index.js
+++ week2/week2-1/index.js
@@ -1,49 +1,45 @@
+const top_btn = document.querySelector(".top_btn");
 
-const top_btn=document.querySelector(".top_btn");
+function handleScroll() {
+  const positionY = window.scrollY;
+  const windowY = window.innerHeight;
 
-function handleScroll(){
-    const positionY=window.scrollY;
-    const windowY=window.innerHeight;
-
-    top_btn.style.opacity=`${positionY/(document.body.clientHeight - 
-        windowY)}`;
+  top_btn.style.opacity = `${
+    positionY / (document.body.clientHeight - windowY)
+  }`;
 }
 
-window.addEventListener("scroll",handleScroll);
-
-
+window.addEventListener("scroll", handleScroll);
 
 function checkSubtextHeight() {
-    let subTextElements = document.querySelectorAll('.subText');
-    subTextElements.forEach(function(subTextElement) {
-
-        if (subTextElement.innerHTML.length > 40) { 
-            subTextElement.nextElementSibling.style.display = 'block'; 
-        }
-    });
+  const subTextElements = document.querySelectorAll(".subText");
+  subTextElements.forEach(function (subTextElement) {
+    if (subTextElement.innerHTML.length > 40) {
+      subTextElement.nextElementSibling.style.display = "block";
+    }
+  });
 }
 
-let allItem = document.querySelector(".all-item__ul");
+const allItem = document.querySelector(".all-item__ul");
 
-
-document.querySelector('.scroll-button.left').addEventListener('click', function() {
-   allItem.scrollTo(
-        {
-            top:0,
-            left:0,
-            behavior: 'smooth' // 부드러운 스크롤
-        }
-        );
-
-});
-
-document.querySelector('.scroll-button.right').addEventListener('click', function() {
-   allItem.scrollTo({
-        top:0,
-        left:document.querySelector('.all-item__ul').scrollWidth,
-        behavior: 'smooth' // 부드러운 스크롤을 위해 추가합니다.
+document
+  .querySelector(".scroll-button.left")
+  .addEventListener("click", function () {
+    allItem.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth", // 부드러운 스크롤
     });
-});
+  });
 
+document
+  .querySelector(".scroll-button.right")
+  .addEventListener("click", function () {
+    allItem.scrollTo({
+      top: 0,
+      left: allItem.scrollWidth,
+      behavior: "smooth", // 부드러운 스크롤을 위해 추가합니다.
+    });
+  });
 
 checkSubtextHeight();
