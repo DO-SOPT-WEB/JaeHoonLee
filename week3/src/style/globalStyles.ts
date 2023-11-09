13d9efa7cbc5babc27bdb55c2c47344df161d4be
ljh0608 <m98m@catholic.ac.kr>
2023-11-09 14:24:03 +0900

    design: globalstyle 추가

--- week3/src/style/globalStyles.ts
+++ week3/src/style/globalStyles.ts
@@ -4,11 +4,20 @@ import { reset } from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
+    * {
+  box-sizing: border-box;
+    }
     html, body {
 
 margin: 0 auto;
-
 font-size: 62.5%;
+}
+
+button {
+border: none;
+background: none;
+  cursor: pointer;
+font: inherit;
 }
 `;
 
