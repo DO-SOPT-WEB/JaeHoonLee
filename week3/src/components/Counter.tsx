e4e6a1222087cbf17a125df086069e63b8c1e9e8
ljh0608 <m98m@catholic.ac.kr>
2023-11-13 04:27:09 +0900

    fix: type 수정

--- week3/src/components/Counter.tsx
+++ week3/src/components/Counter.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-const Counter = ({ time }: number) => {
+
+const Counter = ({ time }: { time: number }) => {
   return <CounterWrapper>{time}</CounterWrapper>;
 };
 
