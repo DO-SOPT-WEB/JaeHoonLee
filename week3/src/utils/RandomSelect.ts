a5a81a5d614661e16e9efe8b6ca8fb9d84080e81
ljh0608 <m98m@catholic.ac.kr>
2023-11-13 03:50:07 +0900

    fix: FoodType지정

--- week3/src/utils/RandomSelect.ts
+++ week3/src/utils/RandomSelect.ts
@@ -1,4 +1,12 @@
-const getRandomFood = (foods) => {
+interface FoodTypes {
+  name: string;
+  countryType: string;
+  ingredient: string;
+  fried: string;
+  src: string;
+}
+
+const getRandomFood = (foods: FoodTypes[]) => {
   const randomIndex = Math.floor(Math.random() * foods.length);
   return foods[randomIndex];
 };
