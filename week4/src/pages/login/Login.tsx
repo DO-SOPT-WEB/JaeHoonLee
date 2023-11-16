3727a9ab2260e166c6f84e6e80e984933408fc21
ljh0608 <m98m@catholic.ac.kr>
2023-11-17 04:54:00 +0900

    init: styled-components 초기세팅

---
+++ week4/src/pages/login/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+const Login = () => {
+  return (
+    <div>
+      <Test>asd</Test>
+    </div>
+  );
+};
+
+const Test = styled.div`
+  background-color: red;
+`;
+export default Login;
---
+++ week4/src/styles/theme.ts
@@ -0,0 +1,27 @@
+import { DefaultTheme } from "styled-components";
+
+const colors = {
+  mainPurple: "#5c478c",
+  mainWhite: "#ffffff",
+  mainGrey: "#ececec",
+};
+
+const fontSize = {
+  head: "8rem",
+  head0: "3.5rem",
+  head1: "2.8rem",
+  head2: "2.4rem",
+  head3: "2rem",
+  body1: "1.8rem",
+  body2: "1.2rem",
+};
+
+export type ColorTypes = typeof colors;
+export type FontsTypes = typeof fontSize;
+
+const theme: DefaultTheme = {
+  colors,
+  fontSize,
+};
+
+export default theme;
