ea64c53d1758a404886b537b4715ada3dc974700
ljh0608 <m98m@catholic.ac.kr>
2023-11-17 07:11:25 +0900

    feat: useFormInput custom hooks

--- week4/src/pages/login/Login.tsx
+++ week4/src/pages/login/Login.tsx
@@ -1,6 +1,11 @@
 import Button from '../../components/atomComponents/Button';
+import useFormInput from '../../hooks/useFormInput';
 import * as S from './Login.style';
+
 const Login = () => {
+  const userId = useFormInput();
+  const userPwd = useFormInput();
+
   const handleLogin = () => {
     console.log('gd');
   };
@@ -10,12 +15,12 @@ const Login = () => {
         <S.LoginText>Login</S.LoginText>
         <S.LoginFormWrapper>
           <S.InputLabel htmlFor="id">
-            <S.LabelSpan> ID:</S.LabelSpan> <input type="text" />
+            <S.LabelSpan> ID</S.LabelSpan> <input type="text" {...userId} />
           </S.InputLabel>
 
           <S.InputLabel htmlFor="pwd">
-            <S.LabelSpan> PW: </S.LabelSpan>
-            <input type="text" />
+            <S.LabelSpan> PW </S.LabelSpan>
+            <input type="text" {...userPwd} />
           </S.InputLabel>
         </S.LoginFormWrapper>
         <S.LoginBtnWrapper>
