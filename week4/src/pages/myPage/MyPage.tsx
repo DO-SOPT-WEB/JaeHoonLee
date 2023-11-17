9cb4d47dcfce93d6996a1bb9e2d59e1a3835cb40
ljh0608 <m98m@catholic.ac.kr>
2023-11-17 15:26:51 +0900

    fix: mypage UI

--- week4/src/pages/myPage/MyPage.tsx
+++ week4/src/pages/myPage/MyPage.tsx
@@ -1,5 +1,22 @@
+import * as S from './MyPage.style';
+
 const MyPage = () => {
-  return <div>Mypage</div>;
+  return (
+    <S.SignUpWrapper>
+      <S.SignUpTitle> MY PAGE</S.SignUpTitle>
+      <S.SignUpContainer>
+        <S.SignUpImage />
+        <S.UserInfoWrapper>
+          <S.UserInfoContainer>
+            <S.UserInfoText>ID: </S.UserInfoText> <S.UserInfoText> </S.UserInfoText>
+          </S.UserInfoContainer>
+          <S.UserInfoContainer>
+            <S.UserInfoText>닉네임: </S.UserInfoText> <S.UserInfoText> </S.UserInfoText>
+          </S.UserInfoContainer>
+        </S.UserInfoWrapper>
+      </S.SignUpContainer>
+    </S.SignUpWrapper>
+  );
 };
 
 export default MyPage;
--- week4/src/pages/signUp/SignUp.tsx
+++ week4/src/pages/signUp/SignUp.tsx
@@ -1,22 +1,7 @@
 import * as S from './SignUp.style';
 
 const SignUp = () => {
-  return (
-    <S.SignUpWrapper>
-      <S.SignUpTitle> MY PAGE</S.SignUpTitle>
-      <S.SignUpContainer>
-        <S.SignUpImage />
-        <S.UserInfoWrapper>
-          <S.UserInfoContainer>
-            <S.UserInfoText>ID: </S.UserInfoText> <S.UserInfoText> </S.UserInfoText>
-          </S.UserInfoContainer>
-          <S.UserInfoContainer>
-            <S.UserInfoText>닉네임: </S.UserInfoText> <S.UserInfoText> </S.UserInfoText>
-          </S.UserInfoContainer>
-        </S.UserInfoWrapper>
-      </S.SignUpContainer>
-    </S.SignUpWrapper>
-  );
+  return;
 };
 
 export default SignUp;
