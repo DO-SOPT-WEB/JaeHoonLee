97fedb114969c15bbbe5f90d6f194a0a0f494f4e
ljh0608 <m98m@catholic.ac.kr>
2023-11-17 15:23:08 +0900

    style: signUp page UI

--- week4/src/pages/signUp/SignUp.tsx
+++ week4/src/pages/signUp/SignUp.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import * as S from './SignUp.style';
 
 const SignUp = () => {
-  return <div>SiginUp</div>;
+  return (
+    <S.SignUpWrapper>
+      <S.SignUpTitle> MY PAGE</S.SignUpTitle>
+      <S.SignUpContainer>
+        <S.SignUpImage />
+        <S.UserInfoWrapper>
+          <S.UserInfoContainer>
+            <S.UserInfoText>ID: </S.UserInfoText> <S.UserInfoText> </S.UserInfoText>
+          </S.UserInfoContainer>
+          <S.UserInfoContainer>
+            <S.UserInfoText>닉네임: </S.UserInfoText> <S.UserInfoText> </S.UserInfoText>
+          </S.UserInfoContainer>
+        </S.UserInfoWrapper>
+      </S.SignUpContainer>
+    </S.SignUpWrapper>
+  );
 };
 
 export default SignUp;
