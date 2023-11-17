fd7a5ed8765cc5bb8e76b7fd8a77f45c71c31ccc
ljh0608 <m98m@catholic.ac.kr>
2023-11-17 23:59:30 +0900

    feat: 로그인 실패 모달

--- week4/src/pages/login/Login.tsx
+++ week4/src/pages/login/Login.tsx
@@ -4,12 +4,19 @@ import * as S from './Login.style';
 import { client } from '../../apis/client';
 import useRouter from '../../hooks/useRouter';
 import { isAxiosError } from 'axios';
+import { useState } from 'react';
+import ModalPortal from '../../utils/Portal';
+import Modal from '../../components/modal';
 const Login = () => {
+  const [modal, setModal] = useState(false);
+  const [errMsg, setErrMsg] = useState('');
   const userId = useFormInput();
   const userPwd = useFormInput();
 
   const { routeTo } = useRouter();
-
+  const handleModal = () => {
+    setModal(!modal);
+  };
   const handleLogin = async () => {
     try {
       const res = await client.post('/api/v1/members/sign-in', {
@@ -23,7 +30,9 @@ const Login = () => {
     } catch (err) {
       if (isAxiosError(err) && err.response) {
         if (err.response.status == 400) {
-          alert('아이디 혹은 비밀번호가 틀립니다.');
+          // alert('아이디 혹은 비밀번호가 틀립니다.');
+          setModal(true);
+          setErrMsg('에러');
         } else {
           console.log(err.response.status);
         }
@@ -57,6 +66,9 @@ const Login = () => {
           </Button>
         </S.LoginBtnWrapper>
       </S.LoginWrapper>
+      <ModalPortal>
+        {modal && <Modal onClose={handleModal} text={'ㅁㄴㅇㅁㄴㅇ'}></Modal>}
+      </ModalPortal>
     </>
   );
 };
