c1f73923c8cf3975355dba6be8cf1c8503d215c3
ljh0608 <m98m@catholic.ac.kr>
2023-11-17 16:42:44 +0900

    feat: getUserInfo api연결

--- week4/src/pages/myPage/MyPage.tsx
+++ week4/src/pages/myPage/MyPage.tsx
@@ -1,6 +1,24 @@
 import * as S from './MyPage.style';
-
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { client } from '../../apis/client';
 const MyPage = () => {
+  const [data, setData] = useState();
+  const { userId } = useParams();
+
+  const getUserInfo = async () => {
+    try {
+      const res = await client.get(`/api/v1/members/${userId}`);
+      console.log(res.data);
+      setData(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
   return (
     <S.MyPageWrapper>
       <S.MyPageTitle> MY PAGE</S.MyPageTitle>
@@ -8,10 +26,11 @@ const MyPage = () => {
         <S.MyPageImage />
         <S.UserInfoWrapper>
           <S.UserInfoContainer>
-            <S.UserInfoText>ID: </S.UserInfoText> <S.UserInfoText> </S.UserInfoText>
+            <S.UserInfoText>ID: </S.UserInfoText> <S.UserInfoText>{data?.username}</S.UserInfoText>
           </S.UserInfoContainer>
           <S.UserInfoContainer>
-            <S.UserInfoText>닉네임: </S.UserInfoText> <S.UserInfoText> </S.UserInfoText>
+            <S.UserInfoText>닉네임: </S.UserInfoText>
+            <S.UserInfoText> {data?.nickname}</S.UserInfoText>
           </S.UserInfoContainer>
         </S.UserInfoWrapper>
       </S.MyPageContainer>
